4e7c44ac21517e95c8e03c130fed32e8c98b1c45
Mateus Hamade <mateus.hamade@aluno.ufop.edu.br>
2022-04-18 10:45:12 -0300

    menu de acesso às pesquisas

--- src/main.c
+++ src/main.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
 
-#include "sequencial.h"
-#include "binaria.h" 
 #include "b.h"
 #include "b_estrela.h"
+#include "binaria.h"
+#include "sequencial.h"
 
-int main(int argc, char const *argv[])
-{
-	printf("TP1 - Estrutura de Dados II");
-	//Implementar menu de acesso às pesquisas
-	return 0;
+// 1 - acesso sequencial
+// 2 - árvore binaria
+// 3 - árvore B
+// 4 - árvore B estrela
+
+int main(int argc, char const *argv[]) {
+    printf("TP1 - Estrutura de Dados II\n");
+
+    printf("Digite o tipo de pesquisa desejada:\n");
+    int metodo = atoi(argv[1]);
+
+    // Implementar menu de acesso às pesquisas
+    switch (metodo) {
+        case 1:  // acesso sequencial
+            sequencial();
+            break;
+        case 2:  // árvore binaria
+            binaria();
+            break;
+        case 3:  // árvore B
+            b();
+            break;
+        case 4:  // árvore B estrela
+            b_estrela();
+            break;
+        default:  // opção inválida
+            printf("Opção inválida!\n");
+            break;
+    }
+
+    return 0;
 }
