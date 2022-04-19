0d6444460578f1cbb851651e03ba5f208b55cde0
Diego Demétrio <diego.demetrio@pdcase.com>
2022-04-19 10:40:30 -0300

    inclusao digital

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "b.h"
 #include "b_estrela.h"
--- src/sequencial.h
+++ src/sequencial.h
@@ -1,5 +1,6 @@
 #ifndef sequencial_h
 #define sequencial_h
+#include <stdio.h>
 
 //definir cabeçalho de função para pesquisa sequencial
 
