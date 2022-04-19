b5c076c06f334ce150a5f066666734a3ca250c33
Guilherme1258 <selio.filho@aluno.ufop.edu.br>
2022-04-19 11:45:33 -0300

    Somente Cópia

--- src/b_estrela.c
+++ src/b_estrela.c
@@ -1,1 +1,28 @@
 #include "b_estrela.h"
+
+void Pesquisa(TipoRegistro *x, TipoApontador *Ap) {
+    int i;
+    TipoApontador Pag;
+    Pag = *Ap;
+
+    if((*Ap)->Pt == Interna) {
+        i =1;
+
+        while(i < Pag->UU.U0.ni && x->chave > Pag->UU.U0.ri[i - 1]) i++;
+
+        if(x->chave < Pag->UU.U0.ri[i - 1])
+            Pesquisa(x, &Pag->UU.U0.pi[i - 1]);
+        else
+            Pesquisa(x, &Pag->UU.U0.pi[i]);
+        
+        return;        
+    }
+
+    i = 1;
+    while(i < Pag->UU.U1.ne && x->chave > Pag->UU.U1.re[i - 1].Chave) i++;
+
+    if(x->chave == Pag->UU.U1.re[i - 1].chave)
+        *x = Pag->UU.U1.re[i - 1];
+    else 
+        printf("TipoRegistro nao esta presente na arvore\n");
+}
--- src/b_estrela.h
+++ src/b_estrela.h
@@ -1,6 +1,39 @@
 #ifndef b_estrela_h
 #define b_estrela_h
 
+#define TAM_STRING 5000  
+
 //definir cabeçalho de função para pesquisa em arvore b_estrela
+typedef long TipoChave;
+
+typedef struct TipoRegistro {
+    TipoChave chave;
+    // Pretendo mudar os nomes
+    long dado1;
+    char dado2[TAM_STRING];
+} TipoRegistro;
+
+typedef enum {Interna, Externa} TipoIntExt;
+typedef struct TipoPagina* TipoApontador;
+
+typedef struct TipoPagina {
+    TipoIntExt Pt;
+    union {
+        struct {
+            int ni;
+            TipoChave ri[MM];
+            TipoApontador pi[MM + 1];
+        } U0;
+        struct {
+            int ne;
+            TipoRegistro re[MM * 2];
+        } U1;
+    } UU;
+} TipoPagina;
+
+void Pesquisa(TipoRegistro *x, TipoApontador *Ap);
+
+
+
 
 #endif
